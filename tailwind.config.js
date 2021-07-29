5ed206db8ad18259bc1c128e7c07dc1165fb5f2f
Rio Purnomo <siriokun@gmail.com>
2021-07-30 00:22:01 +0700

    Add Poppins font to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   mode: 'jit',
   purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}'],
+  theme: {
+    fontFamily: {
+      'body': '"Poppins", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+    }
+  }
 };
